5b30b8518eef369f249c25d533a0a0fe84be5062
Ihor08 <ihor100500@ukr.net>
2020-04-24 16:28:07 +0300

    first program

---
+++ Lab2_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Lab2_1
+{
+    class Class1
+    {
+        static void Main()
+        {
+
+            double a = 3;
+            double b = 6;
+            double dx = 0.05;
+            double x = a;
+            double y = 0;
+
+            while (x < b)
+            {
+                y = Math.Pow(2.71, Math.Sqrt(2 * x)) * x * x;
+
+
+                Console.WriteLine("x = " + x + "  " + "y = " + y);
+
+
+
+                x += dx;
+            }
+
+
+        }
+
+    }
+}
